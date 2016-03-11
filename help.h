17d89fa4e59d0a808710a522fcccdc301c90bfa5
Pavel Korozevtsev <paxakor@yandex.ru>
2016-03-11 18:27:30 +0300

    default values added in help message changed name of executable to "wrp"

--- help.h
+++ help.h
@@ -4,10 +4,10 @@ const char* help_msg_ru =
 "Эта программа разбивает текст на строки длины не более заданной.\n\
 В основном, предназначается для форматирования исходных файлов LaTeX.\n\
 Использование: wrp [options]\n\
-Options:\n\
+Options:       Description                           Default value\n\
   -h, --help   print this message and exit\n\
-  -i <File>    set File as input\n\
-  -o <File>    set File as output\n\
-  -d <Symbol>  set Symbol as delimeter\n\
-  -l <Number>  set Number as max length of the line\n\
-  -t <Number>  set Number as tab size\n";
+  -i <File>    set File as input                     stdin\n\
+  -o <File>    set File as output                    stdout\n\
+  -d <Symbol>  set Symbol as delimeter               ' ' (space, 32)\n\
+  -l <Number>  set Number as max length of the line  80\n\
+  -t <Number>  set Number as tab size                0\n";
